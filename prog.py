7d421c3d6231ee95354c4e0cf63adc4b1535fbdd
Dima Baranov <baranovdima42@gmail.com>
2026-03-27 20:15:58 +0500

    END

--- prog.py
+++ prog.py
@@ -1,6 +1,5 @@
 import sqlite3
 
-#Устанавливаем соединение с базой данных
 connection = sqlite3.connect('prodykt_db.db')
 cursor = connection.cursor()
 
@@ -13,6 +12,11 @@ age INTEGER
 )
 ''')
 connection.commit()
+
+cursor.execute('SELECT * FROM Users')
+users = cursor.fetchall()
+for user in users:
+	print(user)
 
 cursor.execute('INSERT INTO Users (ussername, email, age) VALUES (?, ?, ?)', ('newuser', 'newuser@example.com', 28))
 connection.commit()
@@ -37,5 +41,44 @@ cursor.execute('SELECT * FROM Users')
 users = cursor.fetchall()
 for user in users:
 	print(user)
-#Сохраняем изменения и закрываем соединение
+
+first_user = cursor.fetchone()
+print(first_user)
+
+first_five_user = cursor.fetchmany(5)
+print(first_five_user)
+
+all_user = cursor.fetchall()
+print(all_user)
+
+# Создаем таблицу Tasks
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Tasks ( id INTEGER PRIMARY KEY,
+title TEXT NOT NULL,
+status TEXT DEFAULT 'Not Started'
+)
+''')
+# Функция для добавления новой задачи
+def add_task(title):
+	cursor.execute('INSERT INTO Tasks (title) VALUES(?)', (title,))
+	connection.commit()
+# Функция для обновления статуса задачи
+def update_task_status(task_id, status):
+	cursor.execute('UPDATE Tasks SET status = ? WHERE id = ?', (status, task_id))
+	connection.commit()
+# Функция для вывода списка задач
+def list_tasks():
+	cursor.execute('SELECT * FROM Tasks')
+	tasks = cursor.fetchall()
+	for task in tasks:
+		print(task)
+# Добавляем задачи
+add_task('Подготовить презентацию')
+add_task('Закончить отчет')
+add_task('Подготовить ужин')
+# Обновляем статус задачи 
+update_task_status(2, 'In Progress')
+# Выводим список задач
+list_tasks()
+# Закрываем соединение
 connection.close()
