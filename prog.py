3fd607935dd70d03ddfdf2bc3c9f54653404096c
Dima Baranov <baranovdima42@gmail.com>
2026-03-27 20:03:26 +0500

    Добавление, обновление, удаление

--- prog.py
+++ prog.py
@@ -4,8 +4,38 @@ import sqlite3
 connection = sqlite3.connect('prodykt_db.db')
 cursor = connection.cursor()
 
-#Создаем индекс для столбца 'email'
-cursor.execute('CREATE INDEX idx_email ON Users (email)')
-#Сохраняем изменения и закрываем соединение
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users (
+id INTEGER PRIMARY KEY,
+ussername TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER 
+)
+''')
 connection.commit()
+
+cursor.execute('INSERT INTO Users (ussername, email, age) VALUES (?, ?, ?)', ('newuser', 'newuser@example.com', 28))
+connection.commit()
+
+cursor.execute('SELECT * FROM Users')
+users = cursor.fetchall()
+for user in users:
+	print(user)
+
+cursor.execute('UPDATE Users SET age = ? WHERE ussername = ?', (29, 'newuser'))
+connection.commit()
+
+cursor.execute('SELECT * FROM Users')
+users = cursor.fetchall()
+for user in users:
+	print(user)
+
+cursor.execute('DELETE FROM Users WHERE ussername =?', ('newuser',))
+connection.commit()
+
+cursor.execute('SELECT * FROM Users')
+users = cursor.fetchall()
+for user in users:
+	print(user)
+#Сохраняем изменения и закрываем соединение
 connection.close()
